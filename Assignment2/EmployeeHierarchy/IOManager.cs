c9228a6726f49ce237a6c17f149e2965bd6f39bd
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-04-24 15:15:14 +0530

    feat: cs: add manager class

---
+++ Assignment2/EmployeeHierarchy/IOManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeHierarchy
+{
+    internal class IOManager
+    {
+        public static string GetInput(string parameter)
+        {
+            Console.Write($"Enter the {parameter}: ");
+            string userInput = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(userInput) || userInput.Any(char.IsDigit))
+            {
+                Console.Write($"Enter the {parameter}: ");
+                Console.WriteLine("Invalid input! Please enter a decimal or integer.");
+                userInput = Console.ReadLine();
+            }
+            return userInput;
+        }
+        public static decimal GetSalary()
+        {
+            Console.Write("Enter the salary: ");
+            string userInput = Console.ReadLine();
+            decimal parsedInput;
+            while (!decimal.TryParse(userInput, out parsedInput))
+            {
+                Console.Write("Enter the salary: ");
+                Console.WriteLine("Invalid input! Please enter a decimal or integer.");
+                userInput = Console.ReadLine();
+            }
+            return parsedInput;
+        }
+
+        public static void PrintEmployeeDetails(string name, decimal salary)
+        {
+            Console.WriteLine($"Name: {name} \nSalary: {salary}");
+        }
+    
+    }
+
+}
---
+++ Assignment2/EmployeeHierarchy/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeHierarchy
+{
+    internal class Manager : Employee
+    {
+        public void GetName()
+        {
+            name = IOManager.GetInput("name");
+        }
+        public void GetSalary()
+        {
+            salary = IOManager.GetSalary();
+        }
+
+        public override decimal CalculateSalary()
+        {
+            return salary * (decimal) 0.05;
+        }
+
+        public void PrintDetails()
+        {
+            IOManager.PrintEmployeeDetails(name, salary);
+        }
+    }
+}
