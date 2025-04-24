e68149026a418f54c452fac2ca11875d8ee1928b
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-04-24 15:16:30 +0530

    feat: cs: add developer class

---
+++ Assignment2/EmployeeHierarchy/Developer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EmployeeHierarchy
+{
+    internal class Developer : Employee
+    {
+        public void GetName()
+        {
+            name = IOManager.GetInput("name");
+        }
+        public void GetSalary()
+        {
+            salary = IOManager.GetSalary();
+        }
+
+        public override decimal CalculateSalary()
+        {
+            return salary * (decimal)0.02;
+        }
+
+        public void PrintDetails()
+        {
+            IOManager.PrintEmployeeDetails(name, salary);
+        }
+    }
+}
