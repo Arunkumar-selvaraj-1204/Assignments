f7d1b82d9e04c374c6bb1e57444e9c2b5aff012a
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-04-24 16:05:51 +0530

    feat: cs: add bank account class

---
+++ Assignment2/BankingSystem/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    internal abstract class BankAccount
+    {
+        public string accountNumber { get; set; }
+        public decimal balance { get; set; }
+
+        public void DepositAmount(decimal amount)
+        {
+            balance += amount;
+        }
+        public abstract void WithdrawAmount();
+    }
+}
