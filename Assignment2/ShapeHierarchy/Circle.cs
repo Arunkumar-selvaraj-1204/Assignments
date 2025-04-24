917fbb16a570d1b5497776a75e4189727f8504b6
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-04-24 11:17:01 +0530

    feat: cs: add circle class

---
+++ Assignment2/ShapeHierarchy/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeHierarchy
+{
+    internal class Circle: Shape
+    {
+        private double _radius;
+
+        public void GetRadius()
+        {
+            _radius = InputManager.GetInput("Radius");
+        }
+        public void GetColor()
+        {
+            color = InputManager.GetColor();
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI*_radius*_radius;
+        }
+
+        public void PrintDetails()
+        {
+            OutputManager.PrintShapeDetails(color, "Circle", CalculateArea());
+        }
+    }
+}
