587e74e6806c8124506f1ddd08b2b9a589dd956e
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-04-24 09:53:11 +0530

    feat: cs: create shape class

---
+++ Assignment2/ShapeHierarchy/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeHierarchy
+{
+    internal abstract class Shape
+    {
+        string color = "Red";
+        public abstract int CalculateArea();
+        public void PrintDetails()
+        {
+            Console.WriteLine("It a generic shape");
+        }
+
+    }
+}
