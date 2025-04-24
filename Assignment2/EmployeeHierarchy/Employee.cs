79e1cb207ccae270ea0ccd2adb787fab567357a4
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-04-24 14:50:29 +0530

    feat: cs: add employee class

---
+++ Assignment2/EmployeeHierarchy/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeHierarchy
+{
+    internal abstract class Employee
+    {
+        public string name { get; set; }
+        public decimal salary { get; set; }
+        public abstract decimal CalculateSalary();
+        public void PrintDetails()
+        {
+            Console.WriteLine("It is generic employee");
+        }
+
+    }
+}
