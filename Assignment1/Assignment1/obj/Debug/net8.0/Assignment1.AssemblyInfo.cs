48b0845b678ccfa80825d82f9468c4a3d015f3af
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-04-25 08:40:26 +0530

    Fix: remove unwnated files

--- Assignment1/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0827677f7377a23ba453b09a873f1eb134bbad16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c3c85eba0168bc850d3f8f3969d384b45c38c74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
