125fef64f1ef3d6257f57d221cc308ab265e1ac4
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-05-12 15:17:51 +0530

    feat: add suplier details for the product using inner join

--- LanguageIntegratedQuery/LanguageIntegratedQuery/Task2.cs
+++ LanguageIntegratedQuery/LanguageIntegratedQuery/Task2.cs
@@ -20,14 +20,12 @@ namespace LanguageIntegratedQuery
         {
             RunTask2_1();
             RunTask2_2();
-
-
          }
 
         private void RunTask2_1()
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Task 2.1: Grouped by category and its expensive product detail.");
+            Console.WriteLine("\n\nTask 2.1: Grouped by category and its expensive product detail.");
             Console.ResetColor();
 
             Console.WriteLine("{0,-15} {1,-10} {2,-20} {3,10}", "Category", "Count", "Expensive Product", "Price");
@@ -50,7 +48,48 @@ namespace LanguageIntegratedQuery
         }
         private void RunTask2_2()
         {
-            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n\nTask 2.2: Print supplier name of the products.");
+            Console.ResetColor();
+
+            List<Supplier> supplierList = GenerateSupplierList();
+            IEnumerable<(int productId, string productName, string supplierName)> supplierOfProducts = _products.Join(supplierList, product => product.ProductId, supplier => supplier.ProductId, (product, supplier) => (product.ProductId, product.ProductName, supplier.SupplierName));
+            Console.WriteLine("{0,-10} {1,-25} {2,-25}", "ProductId", "ProductName", "SupplierName");
+            Console.WriteLine(new string('-', 65));
+            foreach (var productData in supplierOfProducts)
+            {
+                Console.WriteLine("{0,-10} {1,-25} {2,-25}",
+                    productData.productId,
+                    productData.productName,
+                    productData.supplierName);
+            }
+        }
+
+        private List<Supplier> GenerateSupplierList()
+        {
+            List <Supplier> suppliers = new List<Supplier>()
+            {
+                new  Supplier{SupplierId = 1, SupplierName = "Supplier 1", ProductId = 15 },
+                new  Supplier{SupplierId = 1, SupplierName = "Supplier 1", ProductId = 12 },
+                new  Supplier{SupplierId = 1, SupplierName = "Supplier 1", ProductId = 10 },
+                new  Supplier{SupplierId = 2, SupplierName = "Supplier 2", ProductId = 14 },
+                new  Supplier{SupplierId = 2, SupplierName = "Supplier 2", ProductId = 13 },
+                new  Supplier{SupplierId = 3, SupplierName = "Supplier 3", ProductId = 11 },
+                new  Supplier{SupplierId = 3, SupplierName = "Supplier 3", ProductId = 9 },
+                new  Supplier{SupplierId = 3, SupplierName = "Supplier 3", ProductId = 8 },
+                new  Supplier{SupplierId = 4, SupplierName = "Supplier 4", ProductId = 2 },
+                new  Supplier{SupplierId = 5, SupplierName = "Supplier 5", ProductId = 7 },
+                new  Supplier{SupplierId = 5, SupplierName = "Supplier 5", ProductId = 6 },
+                new  Supplier{SupplierId = 5, SupplierName = "Supplier 5", ProductId = 4 },
+                new  Supplier{SupplierId = 5, SupplierName = "Supplier 5", ProductId = 5 },
+                new  Supplier{SupplierId = 5, SupplierName = "Supplier 5", ProductId = 3 },
+                new  Supplier{SupplierId = 5, SupplierName = "Supplier 5", ProductId = 1 },
+            };
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    suppliers.Add(new Supplier {SupplierId = i, SupplierName = $"Supplier {i}", ProductId = (11-i) });
+            //}
+            return suppliers;
         }
     }
 
