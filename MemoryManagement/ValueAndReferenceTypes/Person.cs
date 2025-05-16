6d0608e57cf79ae10b8d431b8615f591b4a14366
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-05-16 15:18:57 +0530

    feat: implement value and reference type task

---
+++ MemoryManagement/ValueAndReferenceTypes/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace ValueAndReferenceTypes
+{
+    public class Person
+    {
+        /// <summary>
+        /// To get and set the name of the person
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// To get and set the age of the person
+        /// </summary>
+        public int Age { get; set; }
+    }
+}
---
+++ MemoryManagement/ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace ValueAndReferenceTypes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = 4;
+            Person person = new Person();
+            person.Name = "Arun";
+            person.Age = 21;
+
+            Console.WriteLine("-------- Memory Management in C# --------");
+            Console.WriteLine("-------- Value and Reference types --------");
+
+            // Print the value of integer and object member before function call
+            Console.WriteLine($"Value of value type before function call is {number}.\nValue of reference type before function call is {person.Age}.");
+
+            IncrementByOne(number, person);
+
+            // Print the value of integer and object member after function call
+            Console.WriteLine($"\nValue of value type after function call is {number}.\nValue of reference type after function call is {person.Age}.");
+
+            Console.WriteLine("\n-------- Working with integers --------");
+            // Call the function dealing with numbers
+            SumOfIntegers();
+
+            Console.WriteLine("\n-------- Working with array of integers --------");
+            // Call the function dealing with an array of number
+            SumOfArrayElements();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Function to increment the number and person's age by one
+        /// </summary>
+        /// <param name="number"> Represents the integer number </param>
+        /// <param name="person"> Represents the person object </param>
+        public static void IncrementByOne(int number, Person person)
+        {
+            number++;
+            Console.WriteLine($"\nInside the function, the value of value type is {number}.");
+            person.Age++;
+        }
+
+        /// <summary>
+        /// Method to calculate the sum of entered numbers by accessing the value type integers
+        /// </summary>
+        public static void SumOfIntegers()
+        {
+            Console.Write("Enter the count of numbers: ");
+            int countOfNumbers = GetInteger();
+            int sum = 0;
+
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+                Console.Write("Enter the number: ");
+                sum += GetInteger();
+            }
+            Console.WriteLine($"Sum of given integers is {sum}.");
+        }
+
+        /// <summary>
+        /// Method to calculate the sum of entered numbers by accessing the reference type array of integers
+        /// </summary>
+        public static void SumOfArrayElements()
+        {
+            Console.Write("Enter the size of the array: ");
+            int arrayLength = GetInteger();
+            int[] arrayOfNumbers = new int[arrayLength];
+            int sum = 0;
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.Write("Enter the number: ");
+                arrayOfNumbers[i] = GetInteger();
+                sum += arrayOfNumbers[i];
+            }
+
+            Console.WriteLine($"Sum of array elements is {sum}.");
+        }
+
+        /// <summary>
+        /// Method to get valid integer input from the user
+        /// </summary>
+        /// <returns> Returns the valid parsed integer </returns>
+        public static int GetInteger()
+        {
+            string input = Console.ReadLine();
+            int validInteger;
+            while (!int.TryParse(input, out validInteger))
+            {
+                Console.Write("Enter valid number: ");
+                input = Console.ReadLine();
+            }
+            return validInteger;
+        }
+    }
+}
