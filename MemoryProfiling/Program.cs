cf88fadd0f18f499a34e8a1220a7d30205b97b46
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-05-20 11:13:37 +0530

    feat: complete task 1

---
+++ MemoryProfiling/Program.cs
@@ -0,0 +1,13 @@
+﻿using MemoryProfiling.Task1;
+namespace MemoryProfiling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryEater task1 = new MemoryEater();
+            task1.Allocate();
+        }
+    }
+
+}
---
+++ MemoryProfiling/Task1/MemoryEater.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryProfiling.Task1
+{
+    public class MemoryEater
+    {
+        List<int[]> memalloc = new List<int[]>();
+
+        public void Allocate()
+        {
+            while (true)
+            {
+                memalloc.Add(new int[1000]);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
