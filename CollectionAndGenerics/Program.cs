8a0c2426761797009a525e6cc9d26ffc935e342b
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-05-23 16:52:15 +0530

    feat: complete task 1

---
+++ CollectionAndGenerics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using CollectionAndGenerics.Task1;
+namespace CollectionAndGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkingWithList task1 = new WorkingWithList();
+            task1.ExecuteListOperations();
+        }
+    }
+}
