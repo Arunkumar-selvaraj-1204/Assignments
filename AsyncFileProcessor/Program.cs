4dbf1514440bbbd773b0356d20b7d424e25ca21d
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-06-03 16:29:45 +0530

    feat: add async file handling methods

---
+++ AsyncFileProcessor/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AsyncFileProcessor
+{
+        internal class Program
+        {
+            static async Task Main(string[] args)
+            {
+                AsyncFileDataHandler handler = new AsyncFileDataHandler();
+
+                var filesToProcess = new List<(string, string)>
+            {
+                ("file1.txt", "file1_output.txt"),
+                ("file2.txt", "file2_output.txt"),
+                ("file3.txt", "file3_output.txt")
+            };
+
+                Console.WriteLine("Starting asynchronous file processing...");
+                await handler.ProcessMultipleFilesAsync(filesToProcess);
+                Console.WriteLine("All files processed.");
+            }
+        
+    }
+}
