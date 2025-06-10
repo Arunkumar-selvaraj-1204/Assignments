de9efeeec05b530c30ba6d8a7e5f04b6e6e3d6f0
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-06-10 10:39:39 +0530

    feat: check difference between var and dynamic

--- NotificationApp/Program.cs
+++ NotificationApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace NotificationApp
+﻿using System;
+
+namespace NotificationApp
 {
     internal class Program
     {
---
+++ TypeChangerApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace UseOfDynamicAndVar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string message = Console.ReadLine() ?? "Default string";
+            var valueChangingVariable = message;
+            Console.WriteLine($"Value of the variable declared with 'var' keyword is - {valueChangingVariable}");
+
+            // The below line when executed will show the error "Cannot implicitly convert type 'int' to 'string'" denoting that the type of the variables declared with 'var' keyword cannot be changed.
+            // valueChangingVariable = 90;
+            // Console.WriteLine(valueChangingVariable);
+
+            dynamic dynamicVariable = message;
+            Console.WriteLine($"Initial value of the variable declared with 'dynamic' keyword is - {dynamicVariable} and it's type is {dynamicVariable.GetType()}");
+
+            dynamicVariable = 20.543;
+            Console.WriteLine($"Modified value of the variable declared with 'dynamic' keyword is - {dynamicVariable} and it's type is {dynamicVariable.GetType()}");
+            Console.WriteLine("For more details about var and dynamic keyword, Please checkout README.md :)");
+        }
+    }
+}
