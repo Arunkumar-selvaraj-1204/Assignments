902136ae9da5bbe381137df3bd7d63f31a593ace
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-06-10 12:27:29 +0530

    feat: create product class

---
+++ SortProducts/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SortProducts
+{
+    public delegate int SortDelegate (Product firstProduct, Product secondProduct);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        public static int SortByName(Product firstProduct, Product secondProduct)
+        {
+            return String.Compare(firstProduct.ProductName, secondProduct.ProductName);
+        }
+
+        public static int SortByCategory(Product firstProduct, Product secondProduct)
+        {
+            return String.Compare(firstProduct.Category, secondProduct.Category);
+        }
+
+        public static int SortByPrice(Product firstProduct, Product secondProduct)
+        {
+            return firstProduct.Price.CompareTo(secondProduct.Price);
+        }
+    }
+}
