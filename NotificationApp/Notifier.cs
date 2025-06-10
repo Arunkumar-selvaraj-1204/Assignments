fb177d914ec1328873c0dd58ca8bdd8f536bc919
arunkumar.selvaraj <arunkumar.selvaraj@solitontech.com>
2025-06-10 10:22:02 +0530

    feat: add notifier to display message in console

---
+++ NotificationApp/Notifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationApp
+{
+    public class Notifier
+    {
+        public delegate void Notify(string message);
+        public event Notify OnAction;
+
+        public void TriggerEvent(string message)
+        {
+            OnAction?.Invoke(message);
+        }
+    }
+}
---
+++ NotificationApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace NotificationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notifier notifier = new Notifier();
+            notifier.OnAction += PrintMessage;
+
+            Console.Write("Enter a message to display: ");
+            string message = Console.ReadLine();
+            notifier.TriggerEvent(message);
+        }
+
+        private static void PrintMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
